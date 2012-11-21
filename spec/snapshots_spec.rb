68c6fb6c17a6391b3669b7051ff0da243693c1c0
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-11-21 00:10:18 +0000

    We should be able to set base currency

---
+++ spec/snapshots_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'helper'
+require 'rack/test'
+require 'app'
+
+describe 'the application' do
+  include Rack::Test::Methods
+
+  let(:app)  { Sinatra::Application }
+  let(:json) { Yajl::Parser.new.parse last_response.body }
+
+  it 'returns latest snapshot' do
+    get '/latest'
+    assert last_response.ok?
+  end
+
+  it 'sets base currency' do
+    skip 'pending'
+    get '/latest?base=USD'
+    json['base'].must_equal 'USD'
+  end
+end
