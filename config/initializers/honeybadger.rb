4b6fa38af991d76063ea4eb934be4a18269d3145
Hakan Ensari <me@hakanensari.com>
2014-05-13 11:39:00 +0100

    Remove async delivery

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -3,8 +3,4 @@ require 'honeybadger'
 Honeybadger.configure do |config|
   config.environment_name = App.env
   config.api_key = ENV['HONEYBADGER_API_KEY']
-
-  config.async do |notice|
-    Thread.new { notice.deliver }
-  end
 end
