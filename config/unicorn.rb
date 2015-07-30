fd8b2f7a4a31b4d52d61b14b1734e6110ce9129b
Hakan Ensari <me@hakanensari.com>
2015-07-30 23:44:00 +0200

    Allow configuring number of worker processes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 preload_app true
-worker_processes 4
+worker_processes ENV['WORKER_PROCESSES'] || 4
 timeout 10
 
 before_fork do |_, _|
-  Sequel::DATABASES.each { |db| db.disconnect }
+  Sequel::DATABASES.each(&:disconnect)
 end
