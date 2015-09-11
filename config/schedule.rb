41c585ec3c989d417e131f51ffc2fa8747217737
Hakan Ensari <me@hakanensari.com>
2015-09-11 14:40:01 +0100

    Poll ECB more frequently

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 job_type :rake, 'cd :path && foreman run bundle exec rake :task --silent :output'
 
-every '0 13,14,15,16,17 * * 1-5' do
+every '*/15 13,14,15,16,17 * * 1-5' do
   rake "rates:update"
 end
