0320b810c5901a79f15bcde7ff5a0b6b384f4b5f
Hakan Ensari <hakanensari@gmail.com>
2017-01-03 12:50:54 +0000

    Remove unused logger

--- config/app.rb
+++ config/app.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-require 'logger'
 require 'pathname'
 
 # Encapsulates app configuration
 module App
   class << self
-    attr_reader :logger, :version, :released_at
+    attr_reader :version, :released_at
 
     def env
       ENV['RACK_ENV'] || 'development'
@@ -17,7 +16,6 @@ module App
     end
   end
 
-  @logger = Logger.new(STDOUT)
   @version = `git rev-parse --short HEAD 2>/dev/null`.strip!
   @released_at = `git show -s --format=%ci HEAD`
 end
